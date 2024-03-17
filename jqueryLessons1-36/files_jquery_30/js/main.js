2f60bac2106b620fa85b1ac7a68d90bda79a4992
Максим_Intern <max20088888@mail.ru>
2024-03-17 11:02:31 +1000

    Gallery

--- jqueryLessons1-36/files_jquery_30/js/main.js
+++ jqueryLessons1-36/files_jquery_30/js/main.js
@@ -1,6 +1,6 @@
 $(function (){
     $('.small a').click(function (e){
         $('.big img').hide().attr('src', $(this).attr('href')).fadeIn(1000)
-        e.preventDefault()
+        e.preventDefault()fd
     })
 })
